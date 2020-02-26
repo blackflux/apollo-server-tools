56376249774a72da7f207389dfbd33d40190cfaf
Lukas Siemon <github@blackflux.com>
2020-02-25 16:00:40 -0800

    amend: refactor

--- src/modules/comment-deprecation-extension.js
+++ src/modules/comment-deprecation-extension.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const { GraphQLExtension } = require('graphql-extensions');
 const { GraphQLError } = require('graphql');
 const Joi = require('joi-strict');
@@ -24,11 +23,7 @@ class CommentDeprecationExtension extends GraphQLExtension {
     this.versions = Object.fromEntries(Object.entries(opts.versions).map(([k, v]) => [k, new Date(v)]));
 
     this.version = undefined;
-    this.isDeprecated = false;
-    this.deprecationDate = undefined;
-    this.sunsetDate = undefined;
-    this.isSunset = false;
-    this.isUnsupported = false;
+    this.deprecatedMeta = {};
   }
 
   requestDidStart({ request }) {
@@ -36,39 +31,33 @@ class CommentDeprecationExtension extends GraphQLExtension {
   }
 
   executionDidStart({ executionArgs }) {
-    assert(this.deprecationDate === undefined);
-    const {
-      isDeprecated,
-      deprecationDate,
-      sunsetDate,
-      isSunset,
-      minVersionAccessed
-    } = getDeprecationMeta({
+    this.deprecatedMeta = getDeprecationMeta({
       version: this.version,
       versions: this.versions,
       sunsetDurationInDays: this.sunsetDurationInDays,
       schema: executionArgs.schema,
       ast: executionArgs.document
     });
-    this.isDeprecated = isDeprecated;
-    this.deprecationDate = deprecationDate;
-    this.sunsetDate = sunsetDate;
-    this.isSunset = isSunset;
-    this.isUnsupported = isDeprecated ? pv.test(`${minVersionAccessed} <= ${this.version}`) : false;
   }
 
   willResolveField() {
+    const {
+      isDeprecated,
+      sunsetDate,
+      isSunset,
+      minVersionAccessed
+    } = this.deprecatedMeta;
     // can not throw in executionDidStart(), so doing it here
-    if (this.isUnsupported) {
+    if (isDeprecated === true && pv.test(`${minVersionAccessed} <= ${this.version}`)) {
       throw new GraphQLError(
         `Functionality unsupported for version "${this.version}".`,
         undefined, undefined, undefined, undefined, undefined,
         { code: 'DEPRECATION_ERROR' }
       );
     }
-    if (this.forceSunset === true && this.isSunset === true) {
+    if (this.forceSunset === true && isSunset === true) {
       throw new GraphQLError(
-        `Functionality sunset since "${this.sunsetDate.toUTCString()}".`,
+        `Functionality sunset since "${sunsetDate.toUTCString()}".`,
         undefined, undefined, undefined, undefined, undefined,
         { code: 'DEPRECATION_ERROR' }
       );
@@ -77,12 +66,10 @@ class CommentDeprecationExtension extends GraphQLExtension {
 
   willSendResponse(kwargs) {
     const { graphqlResponse } = kwargs;
-    if (this.isDeprecated) {
+    const { isDeprecated, deprecationDate, sunsetDate } = this.deprecatedMeta;
+    if (isDeprecated === true) {
       const headers = Object.fromEntries(graphqlResponse.http.headers.entries());
-      pv.updateDeprecationHeaders(headers, {
-        deprecationDate: this.deprecationDate,
-        sunsetDate: this.sunsetDate
-      });
+      pv.updateDeprecationHeaders(headers, { deprecationDate, sunsetDate });
       Object.entries(headers)
         .forEach(([k, v]) => graphqlResponse.http.headers.set(k, v));
     }
