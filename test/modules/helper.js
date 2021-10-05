25ce17c122f62c477f69e74caf523fa22a8ea36e
Lukas Siemon <github@blackflux.com>
2021-10-05 09:07:05 -0700

    fix: dep update and refactor

--- test/modules/helper.js
+++ test/modules/helper.js
@@ -1,10 +1,10 @@
-const { ApolloServer } = require('apollo-server');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 const fs = require('smart-fs');
 const path = require('path');
 
 module.exports.loadSchema = () => {
-  const server = new ApolloServer({
+  const schema = makeExecutableSchema({
     typeDefs: fs.smartRead(path.join(__dirname, 'schema.graphql')).join('\n')
   });
-  return server.schema;
+  return schema;
 };
